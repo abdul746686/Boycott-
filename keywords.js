f045bfc2c2ca126769fd03ba1b64eb673d098acb
abdul746686 <mohassanuo@gmail.com>
2025-07-14 15:02:04 +0530

    Update and rename AndroidManifest.xml to keywords.js

---
+++ keywords.js
@@ -0,0 +1,56 @@
+// List of boycotted brands and products (sample - should be expanded)
+const boycottBrands = [
+    // Beverages
+    "Coca-Cola", "Pepsi", "Sprite", "Fanta", "7Up", "Mountain Dew", "Dr Pepper",
+    
+    // Fast Food
+    "McDonald's", "Burger King", "KFC", "Pizza Hut", "Domino's", "Subway", "Starbucks",
+    
+    // Tech
+    "Apple", "iPhone", "MacBook", "iPad", "Microsoft", "Windows", "Xbox", "LinkedIn",
+    "Google", "Android", "YouTube", "Facebook", "Instagram", "WhatsApp", "Meta",
+    "Twitter", "Tesla", "Amazon", "Alexa", "Prime",
+    
+    // Clothing
+    "Nike", "Adidas", "Puma", "Zara", "H&M", "Gap", "Levi's", "Calvin Klein",
+    
+    // Other
+    "Nestle", "KitKat", "Nescafe", "Maggi", "Unilever", "Lipton", "Axe", "Dove",
+    "Colgate", "Palmolive", "Johnson & Johnson", "Pampers", "Tide", "Pringles",
+    "Lays", "Doritos", "Cheetos", "Quaker", "Gatorade", "Tropicana", "Head & Shoulders",
+    "Pantene", "Oral-B", "Gillette", "Braun", "Olay", "Always", "Ariel", "Ben & Jerry's",
+    "Best Foods", "Birds Eye", "Blue Band", "Breyers", "Clear", "Close Up", "Comfort",
+    "Cornetto", "Degree", "Dermalogica", "Dirt Is Good", "Dove Men+Care", "Eskimo",
+    "Frisko", "Glow & Lovely", "Hellmann's", "I Can't Believe It's Not Butter",
+    "Knorr", "Lacta", "Lifebuoy", "Lux", "Magnum", "Maille", "Marmite", "OMO",
+    "Rexona", "Seda", "Sedal", "Skippy", "Slim Fast", "St Ives", "Suave", "Sunlight",
+    "Surf", "Sunsilk", "Tresemme", "Vaseline", "Wall's", "Zwitsal",
+    
+    // Add more brands as needed
+    "Disney", "Marvel", "Pixar", "ESPN", "ABC", "HBO", "CNN", "Warner Bros",
+    "Discovery", "Netflix", "Spotify", "eBay", "PayPal", "Western Union",
+    "Visa", "Mastercard", "American Express", "Expedia", "Booking.com",
+    "Airbnb", "Uber", "Lyft", "Bolt", "Intel", "Dell", "HP", "Lenovo",
+    "Asus", "Acer", "Sony", "PlayStation", "Samsung", "LG", "Panasonic",
+    "Toshiba", "Hitachi", "Philips", "Electrolux", "Whirlpool", "General Electric",
+    "Boeing", "Lockheed Martin", "Raytheon", "Northrop Grumman", "General Dynamics",
+    
+    // Food and Beverage (more)
+    "Heinz", "Kraft", "Oreo", "Cadbury", "Milka", "Toblerone", "Ritz", "Trident",
+    "Halls", "Tang", "Carnation", "Coffee Mate", "Häagen-Dazs", "DiGiorno", "Hot Pockets",
+    "Lean Cuisine", "Purina", "Friskies", "Alpo", "Beneful", "Tidy Cats",
+    
+    // Financial
+    "JPMorgan", "Chase", "Bank of America", "Citigroup", "Wells Fargo", "Goldman Sachs",
+    "Morgan Stanley", "BlackRock", "State Street", "Fidelity", "Vanguard",
+    
+    // Pharmaceuticals
+    "Pfizer", "Moderna", "Johnson & Johnson", "Merck", "Bristol-Myers Squibb", "Eli Lilly",
+    "Abbott", "Amgen", "Gilead", "Biogen",
+    
+    // Add more as they become relevant to the boycott
+];
+
+// Note: This list should be regularly updated with new brands and products
+// that are part of the Islamic boycott movement. The actual implementation
+// would include a more comprehensive and up-to-date list.
